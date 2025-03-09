c1d4c2b805ba18f249cab8cfe68dbb95cc5fe3f4
Andrej Anžlovar <andrej.anzlovar2@gmail.com>
2025-03-09 19:46:13 +0100

    Added metadata to content

--- src/flare_ai_rag/utils/data_maker.py
+++ src/flare_ai_rag/utils/data_maker.py
@@ -7,28 +7,57 @@ from flare_ai_rag.utils.splitter import data_split
 logger = structlog.get_logger(__name__)
 
 
+def format_metadata(meta_data: dict) -> str:
+    result = ""
+    for key in meta_data:
+        result += f"{str(key).upper()}: {meta_data[key]}\n"
+
+    return "<metadata>\n" + result + "<\\metadata>\n\n"
+
+
+def metadatadize(func):
+    def wrap_func(*args, **kwargs) -> list[dict]:
+        output = func(*args, **kwargs)
+        for i, data_block in enumerate(output):
+            metadata_context = format_metadata(data_block["meta_data"])
+            data_block["content"] = metadata_context + data_block["content"]
+            output[i] = data_block
+        
+        return output
+    
+    return wrap_func
+
+
+@metadatadize
 def get_data(file: Path, base_path: Path, overlap: int = 1000) -> list[dict]:
     chunk_size = 10 * overlap
     r = []
     extension = file.suffix
     content = file.read_text()
-    meta_data = {}
     file_name = file.relative_to(base_path).as_posix()
+    meta_data = {"file_name": file_name}
+
+    useful_metadata = ["file_name", "description", "keywords", "tags", "title"]
 
     if extension in (".mdx", ".md"):
         if content.startswith("---"):
             _, mdata, content = content.split("---", 2)
             meta_data.update(yaml.safe_load(mdata))
             content = content.strip()
+
         title = ""
         if content.startswith("# "):
             title, content = content.split("\n", 1)
             content = content.strip()
             title = title.lstrip("# ")
             meta_data["title"] = title
+
+        meta_data = {key : value for key, value in meta_data.items() if key.lower() in useful_metadata}
+
         for section in data_split(content, ["\n# ", "\n## ", "\n### "], chunk_size, overlap):
             r.append({"content": section, "meta_data": meta_data,
                      "file_name": file_name, "type": "answer"})
+
     elif extension in (".js", ".sol", ".py"):
         seps = {
             ".js": ["\nfunction", "\nclass", "\nconst", "\nlet", "\nvar"],
@@ -38,10 +67,12 @@ def get_data(file: Path, base_path: Path, overlap: int = 1000) -> list[dict]:
         for section in data_split(content, seps[extension], chunk_size, overlap):
             r.append({"content": section, "meta_data": meta_data,
                     "file_name": file_name, "type": "code"})
+
     else:
         for section in data_split(content, [], chunk_size, overlap):
             r.append({"content": content, "meta_data": meta_data,
                     "file_name": file_name, "type": "answer"})
+
     rr = []
     for i in r:
         if len(i["content"]) < 30:
@@ -59,6 +90,7 @@ def make_data(data_path: Path) -> None:
     for file in data_path.glob("files/**/*"):
         if not file.is_file():
             continue
+
         logger.info(f"Reading file: {file.name}")
         try:
             data.extend(get_data(file, data_path))
